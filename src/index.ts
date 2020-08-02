732359d449193331765b26ae3497f4fa6521d991
Etienne Dldc <e.deladonchamps@gmail.com>
2020-08-02 22:10:15 +0200

    Upgrade deps

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import {
   Chemin,
   CheminParam as P,
   RouterConsumer,
-  HttpError
+  HttpError,
 } from 'tumau';
 import { BREWERIES } from './data/Brewery';
 import { BEERS } from './data/Beer';
@@ -19,7 +19,7 @@ import { CuidSlugParam } from './utils/CuidSlugParam';
 const ROUTES = {
   home: Chemin.create(),
   brewery: Chemin.create('brewery', P.optional(CuidSlugParam('breweryId'))),
-  beer: Chemin.create('beer', CuidSlugParam('beerId'))
+  beer: Chemin.create('beer', CuidSlugParam('beerId')),
 };
 
 const ROUTES_STR = Object.keys(ROUTES).reduce<{ [key: string]: string }>(
@@ -35,9 +35,9 @@ type Breweries = Array<{
   name: string;
 }>;
 
-const BREWERIES_SMALL: Breweries = BREWERIES.map(brewery => ({
+const BREWERIES_SMALL: Breweries = BREWERIES.map((brewery) => ({
   id: brewery.id,
-  name: brewery.name
+  name: brewery.name,
 }));
 
 const server = TumauServer.create({
@@ -47,51 +47,51 @@ const server = TumauServer.create({
     JsonPackage(),
     RouterPackage([
       Route.GET(ROUTES.home, () => {
-        return JsonResponse.with({
+        return JsonResponse.withJson({
           routes: ROUTES_STR,
           source: 'https://github.com/etienne-sandbox/paris-brewery-api',
           examples: {
             allBreweries: ROUTES.brewery.serialize({
-              breweryId: { present: false }
+              breweryId: { present: false },
             }),
             singleBrewery: ROUTES.brewery.serialize({
-              breweryId: { present: true, value: BREWERIES_SMALL[0].id }
+              breweryId: { present: true, value: BREWERIES_SMALL[0].id },
             }),
             singleBeer: ROUTES.beer.serialize({
-              beerId: BREWERIES[0].beers[0].id
-            })
-          }
+              beerId: BREWERIES[0].beers[0].id,
+            }),
+          },
         });
       }),
-      Route.GET(ROUTES.brewery, tools => {
+      Route.GET(ROUTES.brewery, (tools) => {
         const match = tools
           .readContextOrFail(RouterConsumer)
           .getOrFail(ROUTES.brewery);
         const breweryId = match.breweryId;
         if (breweryId.present === false) {
-          return JsonResponse.with<Breweries>(BREWERIES_SMALL);
+          return JsonResponse.withJson<Breweries>(BREWERIES_SMALL);
         }
-        const brewery = BREWERIES.find(b => b.id === breweryId.value);
+        const brewery = BREWERIES.find((b) => b.id === breweryId.value);
         if (!brewery) {
           throw new HttpError.NotFound();
         }
-        return JsonResponse.with(brewery);
+        return JsonResponse.withJson(brewery);
       }),
-      Route.GET(ROUTES.beer, ctx => {
+      Route.GET(ROUTES.beer, (ctx) => {
         const match = ctx
           .readContextOrFail(RouterConsumer)
           .getOrFail(ROUTES.beer);
-        const beer = BEERS.find(b => b.id === match.beerId);
+        const beer = BEERS.find((b) => b.id === match.beerId);
         if (!beer) {
           throw new HttpError.NotFound();
         }
-        return JsonResponse.with<Beer>(beer);
+        return JsonResponse.withJson<Beer>(beer);
       }),
       Route.all(null, () => {
         throw new HttpError.NotFound();
-      })
+      }),
     ])
-  )
+  ),
 });
 
 const DEFAULT_PORT = 3000;
